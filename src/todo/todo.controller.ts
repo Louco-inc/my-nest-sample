b8793e28d753959317966f90152c199200cfc6e7
MoriShun89 <s.moririn0724@gmail.com>
2024-08-11 15:57:32 +0900

    [feat]todo.controllerにaddメソッド追加

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './create-task.dto';
 
 @Controller('todo')
 export class TodoController {
+  constructor(private prisma: PrismaService) {}
   @Get('list')
   getList() {
     return [
@@ -11,5 +14,16 @@ export class TodoController {
         done: false,
       },
     ];
+  }
+
+  @Post('')
+  async add(@Body() task: CreateTaskDto) {
+    const result = await this.prisma.task.create({
+      data: task,
+    });
+    return {
+      result,
+      status: 'OK',
+    };
   }
 }
